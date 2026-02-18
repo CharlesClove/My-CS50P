cb4a68564c798ec277b97386c869ab0556576508
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-02-18 17:42:50 +0000

    Wed, Feb 18, 2026, 6:42 PM +01:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,10 +1,11 @@
-user_input = input()
 
-def the_deep_anwser():
+
+def the_deep_anwser(user_input):
     if user_input == '42' or user_input.lower() == 'forty-two' or user_input.lower() == 'forty two':
         print('Yes')
-    else
+    else:
         print('No')
 
-the_deep_anwser()
+user_input = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+the_deep_anwser(user_input)
 
