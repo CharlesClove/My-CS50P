5236fb5e5e463a38f326ffbd17b09ae89da55e54
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-02-18 17:39:53 +0000

    Wed, Feb 18, 2026, 6:39 PM +01:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,4 +1,10 @@
 user_input = input()
 
 def the_deep_anwser():
-    if user_input == '42' or user_input.to ==
+    if user_input == '42' or user_input.lower() == 'forty-two' or user_input.lower() == 'forty two':
+        print('Yes')
+    else
+        print('No')
+
+the_deep_anwser()
+
