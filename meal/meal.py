03ee71c831fb59269e18efbdd9ea222390679bdf
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-03-05 21:01:34 +0000

    Thu, Mar 5, 2026, 10:01 PM +01:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,9 +1,15 @@
 def main():
     time = input("What time is it? ")
     prased_time = convert(time)
-    print(prased_time)
-    if prased_time >= 7 and prased_time <= 8:
-        return "Breakfast time!"
+    #print(prased_time)
+    if prased_time >= 7 and prased_time <=8:
+        print("breakfast time")
+    elif prased_time >= 12 and prased_time <=13:
+        print("lunch time")
+    elif prased_time >= 18 and prased_time <=19:
+        print("dinner time")
+    
+
 def convert(time):
     hours, minutes = time.split(":")
     return float(int(hours)+(int(minutes)/60))
