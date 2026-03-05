0723d4172cbc78f57ef6b73b929e7fa8b94c684e
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-03-05 14:00:35 +0000

    Thu, Mar 5, 2026, 3:00 PM +01:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -3,6 +3,7 @@ def math_interpreter(question):
     list_of_characters = question.split(" ")
     if len(list_of_characters) == 3:
         list_of_characters.sort()
+        
         return list_of_characters
     else:
         return "invalid expression"
