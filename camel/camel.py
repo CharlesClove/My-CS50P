a66890b28725eb0877ba04bcd789ab1434782120
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-03-21 00:51:48 +0000

    Sat, Mar 21, 2026, 1:51 AM +01:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,6 +1,9 @@
 def swap_to_snake(txt):
     for iteration in range(len(txt)):
-        
+        if isupper(txt[iteration]):
+            print(txt[iteration])
+            
+
 def main():
     txt = input("camelCase:")
     swap_to_snake(str(txt))
