e03ab3b82d59598236cee43b7f09c1709ba70a59
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-03-21 01:12:23 +0000

    Sat, Mar 21, 2026, 2:12 AM +01:00

--- camel/camel.py
+++ camel/camel.py
@@ -2,12 +2,13 @@ def swap_to_snake(txt):
     for iteration in range(len(txt)):
         snake_text=""
         if txt[iteration].isupper():
-            txt[iteration] = txt[iteration].replace(f"{txt[iteration]}", f"_{txt[iteration].lower()}")
-        snake_text.push
+            snake_text.join(txt[iteration].replace(f"{txt[iteration]}", f"_{txt[iteration].lower()}"))
+        snake_text.join(txt[iteration])
+        print(f"snake_case: {snake_text}")
 
 
 def main():
-    txt = input("camelCase:")
+    txt = input("camelCase: ")
     swap_to_snake(str(txt))
 
 main()
