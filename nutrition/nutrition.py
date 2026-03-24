9462872085838129f3af1da7474f95c79f1d668c
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-03-24 14:04:49 +0000

    Tue, Mar 24, 2026, 3:04 PM +01:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,7 +1,10 @@
 Fruits = [
-    "apple": 130,
-    "avocado":50,
-    "kiwifruit":90,
-    "pear":
-    "sweet cherries":
+    "apple":  130,
+    "avocado": 50,
+    "kiwifruit": 90,
+    "pear": 100,
+    "sweet cherries": 100
 ]
+def is_fruit(s):
+    print(Fruits[0])
+is_fruit('a')
