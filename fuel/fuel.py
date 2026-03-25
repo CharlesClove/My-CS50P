768186e8f5925cf87d2ddf0105439f7a30e564aa
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-03-25 14:47:06 +0000

    Wed, Mar 25, 2026, 3:47 PM +01:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,15 +1,13 @@
 def fuel(percent):
-    
+    print(f"{int(round(percent))}%")
     ...
 def main():
     try:
         amount = input("Fraction: ")
         x, y = amount.split('/')
         amount = (float(x)/float(y))*100
-    except ValueError:
-        print(f"{amount} is not a float'able")
-
-
+    except (ValueError, ZeroDivisionError):
+        print("")
     fuel(amount)
 
 main()
