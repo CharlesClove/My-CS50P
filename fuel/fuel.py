d9d5d9bdf6da5264b0b77bbdf324df0d042fc9ac
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-03-26 13:30:14 +0000

    Thu, Mar 26, 2026, 2:30 PM +01:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -2,21 +2,27 @@ def fuel(amount):
     percent = int(amount)
     if percent >= 99:
         print("F")
+
     elif percent <= 1:
         print("E")
+
     else:
         print(f"{percent}%")
 
+
 def main():
-    try:
-        amount = input("Fraction: ")
-        x, y = amount.split('/')
+    while(True):
+        try:
+            amount = input("Fraction: ")
+            x, y = amount.split('/')
+            try:
+                amount = (float(x)/float(y))*100
+            except ZeroDivisionError:
+                pass
+        except ValueError:
+            pass
 
-        if float(x) >= 0 and float(y) > 0:
-            amount = round((float(x)/float(y))*100)
+        fuel(amount)
 
-    except (ValueError, ZeroDivisionError):
-        pass
-    fuel(amount)
 
 main()
