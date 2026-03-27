4e4981015977c8a5bfc8f6ebb3db05aa488669d5
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-03-27 13:55:32 +0000

    Fri, Mar 27, 2026, 2:55 PM +01:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,13 +1,9 @@
 def main():
     while(True):
         try:
-            order_input = input("Item: ")
-            order = order_input.upper()
-            if order not in Menu.keys():
-                raise ValueError
-            order_value = Menu.get(order)
-            total = total + order_value
-            print(f"Total: ${total:.2f}")
+            counter = 1
+            list_item = input("Item: ")
+            order = append(f"{}{list_item.upper()}")
         except ValueError:
             continue
         except EOFError:
