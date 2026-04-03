6207ab5e5cd0062aef42e23022b592d31f8ec58e
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-03 17:22:05 +0000

    Fri, Apr 3, 2026, 7:22 PM +02:00

---
+++ outdated/outdated.py
@@ -0,0 +1,22 @@
+Months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+def main():
+    while(True):
+        try:
+        except (EOFError, ValueError):
+            break
+
+main()
