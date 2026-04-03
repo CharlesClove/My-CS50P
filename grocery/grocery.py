9795322af32318c1acdcb6f375051f6fe187609b
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-03 07:01:53 +0000

    Fri, Apr 3, 2026, 9:01 AM +02:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,10 +1,10 @@
+g_list = {}
 def main():
     while(True):
         try:
-            g_list = {}
             g_item = input("Input: ")
             if g_item in g_list.keys():
-                print("apple+1")
+                
             elif g_item not in g_list.keys():
                 g_list.setdefault(g_item,1)
         except EOFError:
