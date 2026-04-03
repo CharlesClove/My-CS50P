a0f7ad00a18c2983565d7400875aa20157958049
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-03 18:39:01 +0000

    Fri, Apr 3, 2026, 8:39 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -18,7 +18,7 @@ def main():
 
     while(True):
         try:
-            date_input = re.split(', | |/',input("Date: ")) #using regex I split both dates that use slash spaces, etc
+            date_input = re.split(', | |/',input("Date: ").) #using regex I split both dates that use slash spaces, etc
             #January, 8, 1999 or 1/9/1999 logic for string month, logic for 2 and 3 postition to be a int
             if len(date_input) != 3:
                 raise ValueError
@@ -31,7 +31,7 @@ def main():
                 if int(date_input[0]) > 12 or int(date_input[0]) < 0:
                     raise ValueError
                 print(f"{date_input[2]}-{date_input[0].zfill(2)}-{date_input[1].zfill(2)}") #zfill is better than format as it helps assigning it to var
-                
+
             if date_input[0] in Months:
                 word_to_number_month = str(Months.index(date_input[0])+1)
                 print(f"{date_input[2]}-{word_to_number_month.zfill(2)}-{date_input[1].zfill(2)}")
