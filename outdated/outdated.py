4a17bfcfc63266861cb3fefabe6d5c709845417c
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-03 17:44:19 +0000

    Fri, Apr 3, 2026, 7:44 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -19,12 +19,13 @@ def main():
     while(True):
         try:
             date_input = re.split(', | |/',input("Date: ")) #using regex I split both dates that use slash spaces, etc
-            #January, 8, 1999 or 1/9/1999
+            #January, 8, 1999 or 1/9/1999 logic for string month, logic for 2 and 3 postition to be a int
             if len(date_input) != 3:
                 raise ValueError
 
             if date_input[0] in Months:
-                word_to_number_month = 
+                word_to_number_month = Months.index(date_input[0])
+
 
         except ValueError:
             continue
