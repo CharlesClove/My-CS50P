5472eb8755aeb192a7f8150b46fb4976ca582db5
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-03 06:35:28 +0000

    Fri, Apr 3, 2026, 8:35 AM +02:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,16 +1,10 @@
 def main():
     while(True):
         try:
-            
-            counter = 1
             grocery_list = []
-            list_item = input("Item: ")
-            if old_list_item == list_item:
-                counter += 1
-            grocery_list.append(f"{counter} {list_item.upper()}")
-            for item in grocery_list:
-                print(item)
-            old_list_item = list_item
+            list_item = ("Input: ")
+
+
         except EOFError:
             break
 
