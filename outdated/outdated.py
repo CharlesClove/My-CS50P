b2ae52a3ebe8d482fe7d88dee3651c24e81a1b4e
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-03 18:36:29 +0000

    Fri, Apr 3, 2026, 8:36 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -22,18 +22,21 @@ def main():
             #January, 8, 1999 or 1/9/1999 logic for string month, logic for 2 and 3 postition to be a int
             if len(date_input) != 3:
                 raise ValueError
-            if date_input[0].isdigit():
-                if int(date_input[0]) > 12 or int(date_input[0]) < 0:
-                    raise ValueError
+
 
             if int(date_input[1]) > 31 or int(date_input[1]) < 0:
                 raise ValueError
 
+            if date_input[0].isdigit():
+                if int(date_input[0]) > 12 or int(date_input[0]) < 0:
+                    raise ValueError
+                print(f"{date_input[2]}-{date_input[0].zfill(2)}-{date_input[1].zfill(2)}") #zfill is better than format as it helps assigning it to var
+                
             if date_input[0] in Months:
                 word_to_number_month = str(Months.index(date_input[0])+1)
                 print(f"{date_input[2]}-{word_to_number_month.zfill(2)}-{date_input[1].zfill(2)}")
 
-            print(f"{date_input[2]}-{date_input[0].zfill(2)}-{date_input[1].zfill(2)}") #zfill is better than format as it helps assigning it to var
+
             break
         except ValueError:
             continue
