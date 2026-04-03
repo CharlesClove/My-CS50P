ea0664648e739704fd4e2c279ca0d40f6290829b
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-03 07:12:01 +0000

    Fri, Apr 3, 2026, 9:12 AM +02:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -2,7 +2,9 @@ g_list = {}
 def main():
     while(True):
         try:
-            g_item = input("Input: ")
+            x = input("Input: ")
+            print(x)
+            g_item = x.upper()
             if g_item in g_list.keys():
                 g_list.update({g_item: g_list.get(g_item)+1})
             elif g_item not in g_list.keys():
