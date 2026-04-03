c29e0740b9dfbfaa3318dc8eca3a7941f6382c89
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-03 18:27:07 +0000

    Fri, Apr 3, 2026, 8:27 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -23,14 +23,26 @@ def main():
             if len(date_input) != 3:
                 raise ValueError
 
+            if int(date_input[0]) > 12 or int(date_input[0]) < 0:
+                raise ValueError
+
+            if int(date_input[1]) > 31 or int(date_input[1]) < 0:
+
+
+
             if date_input[0] in Months:
                 word_to_number_month = str(Months.index(date_input[0])+1)
                 print(f"{date_input[2]}-{word_to_number_month.zfill(2)}-{date_input[1].zfill(2)}")
+
+
+
+            print(f"{date_input[2]}-{date_input[0].zfill(2)}-{date_input[1].zfill(2)}") #zfill is better than format as it helps assigning it to var
+            break
         except ValueError:
             continue
         except (EOFError):
             break
 
-    print(f"{date_input[2]}-{date_input[0].zfill(2)}-{date_input[1].zfill(2)}") #zfill is better than format as it helps assigning it to var
+
 
 main()
