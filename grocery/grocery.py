fe7ee83bbb09e2ac8e8b253c21e02588cbd95008
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-03 06:40:15 +0000

    Fri, Apr 3, 2026, 8:40 AM +02:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,8 +1,11 @@
 def main():
     while(True):
         try:
-            grocery_list = {}
-            list_item = ("Input: ")
+            g_list = {}
+            g_item = input("Input: ")
+            for key in g_list:
+                if key == g_item:
+                    
 
 
         except EOFError:
