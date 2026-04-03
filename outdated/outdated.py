d709e22afe8b4d278043deb2aa2a6ab4d390c811
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-03 18:55:06 +0000

    Fri, Apr 3, 2026, 8:55 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -15,14 +15,47 @@ Months = [
 ]
 
 def main():
+    while(True):
+        try:
+            di = input("Date: ").strip()
+            if not di[0].isdigit():
+                m, d, y = di.split(" ")
+                if not d.endswith(',')
+                    continue
+                d = d.replace(',', '')
+                d = int(d)
+                y = int(y)
+                m = m.capitalize()
+                if m in a and (1 <= d <= 31):
+                    print(y, f'{Months.index(m)+1:02}',f'{d:02}', sep="-")
+                    break
+
+        except (EOFError):
+            break
+main()
+
+
+def my_soulution():
 
     while(True):
         try:
-            d_i = input("Date: ).strip()
-            
+            date_input = re.split(', | |/',input("Date: ").strip()) #using regex I split both dates that use slash spaces, etc
+            #January, 8, 1999 or 1/9/1999 logic for string month, logic for 2 and 3 postition to be a int
+            if len(date_input) != 3:
+                raise ValueError
+            if int(date_input[1]) > 31 or int(date_input[1]) < 0:
+                raise ValueError
+            if date_input[0].isdigit():
+                if int(date_input[0]) > 12 or int(date_input[0]) < 0:
+                    raise ValueError
+                print(f"{date_input[2]}-{date_input[0].zfill(2)}-{date_input[1].zfill(2)}") #zfill is better than format as it helps assigning it to var
+            if date_input[0] in Months:
+                word_to_number_month = str(Months.index(date_input[0])+1)
+                print(f"{date_input[2]}-{word_to_number_month.zfill(2)}-{date_input[1].zfill(2)}")
             break
         except ValueError:
             continue
         except (EOFError):
             break
-main()
+
+
