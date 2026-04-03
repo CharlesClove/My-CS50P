1b231734115553af14159e65ebfb115888864def
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-03 18:14:07 +0000

    Fri, Apr 3, 2026, 8:14 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -30,6 +30,6 @@ def main():
         except (EOFError):
             break
 
-    print(f"{date_input[2]}-{date_input[0]:02}-{date_input[1]:02}")
+    print(f"{date_input[2]}-{date_input[0]:}-{date_input[1]:02}")
 
 main()
