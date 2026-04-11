b027c09fa18759d4bded6427d4e15ea572c83b5d
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-11 21:55:02 +0000

    Sat, Apr 11, 2026, 11:55 PM +02:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,5 +1,6 @@
 import emoji
+import re
 inputstr = input("Input: ")#I need to 'catch' string between ::
-str1, str2 = inputstr.split(':')
+print(re.search(':*:', inputstr))
 print(emoji.emojize(f'Output: {inputstr}'))
 
