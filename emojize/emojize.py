360855e9c8f2e27ba215da18cedbdce1e0a3e882
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-11 21:56:13 +0000

    Sat, Apr 11, 2026, 11:56 PM +02:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,6 +1,7 @@
 import emoji
-import re
+def find_dots():
+    
 inputstr = input("Input: ")#I need to 'catch' string between ::
-print(re.search(':*:', inputstr))
+
 print(emoji.emojize(f'Output: {inputstr}'))
 
