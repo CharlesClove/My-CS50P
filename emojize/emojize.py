8abd0289148992c554fdaa56471b112864ed1148
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-13 09:24:54 +0000

    Mon, Apr 13, 2026, 11:24 AM +02:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -2,7 +2,7 @@ import emoji
 import re
 
 inputstr = input("Input: ")#I need to 'catch' string between ::
-match = re.match(r":*:", inputstr)
+match = re.match(r":\d*:", inputstr)
 if match:
     print(match.group())
 else:
