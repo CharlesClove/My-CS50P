920763e0726cdf77c39c2b290c6cb76bcc86c486
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-13 09:32:31 +0000

    Mon, Apr 13, 2026, 11:32 AM +02:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -3,13 +3,13 @@ import re
 
 def matching_str(match):
     if match:
-        print(match.group())
+        return match.group()
     else:
         print("no match")
 
 inputstr = input("Input: ")#I need to 'catch' string between ::
 match = re.match(r":\w*:", inputstr)
-matching_str(match)
+match_pattern = matching_str(match)
 split_str = inputstr.split(match.group())
 print(split_str)
 
