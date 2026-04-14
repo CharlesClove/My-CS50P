786f11d70d6e18a09dacd764edaebdadfb51d15a
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-14 21:10:21 +0000

    Tue, Apr 14, 2026, 11:10 PM +02:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,8 +1,10 @@
 import emoji
 import re
 
-inputstr = input("Input: ")#I need to 'catch' string between ::
-match = re.split(r":\w*:", inputstr)
-print(match)
-\
-
+#inputstr = ":candy: or :ice_cream:?"
+inputstr = input("Input: ").strip() #I need to 'catch' string between ::
+match = re.split(r"(:\w+:)", inputstr) #I like re
+new_list = [] # new list to append with emojized words. probably could be done better.
+for word in match:
+    new_list.append(emoji.emojize(word, language = "alias"))
+print(f"Output: {"".join(new_list)}") #joins new_list elements into single string
