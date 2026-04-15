b6cffcb991e60ce9cf1b56fb93b867636b59ddaa
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-15 14:19:49 +0000

    Wed, Apr 15, 2026, 4:19 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -4,8 +4,9 @@ import pyfiglet
 
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('x',type=int) #must be used, the int is accessed in args.x
-praser.add_argument('--optional', help='its purely optional to use it', type=str, default=0, action="count")
-args = parser.parse_args()
-print(args.x)
+# parser = argparse.ArgumentParser()
+# parser.add_argument('x',type=int) #must be used, the int is accessed in args.x
+# praser.add_argument('--optional', help='its purely optional to use it', type=str, default=0, action="count")
+# args = parser.parse_args()
+# print(args.x)
+
