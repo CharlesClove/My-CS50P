60dd6c857deab2390aef254b38ab727edcfb982b
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-15 14:48:12 +0000

    Wed, Apr 15, 2026, 4:48 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -15,11 +15,11 @@ parser = argparse.ArgumentParser()
 # praser.add_argument('--optional', help='its purely optional to use it', type=str, default=0, action="count")
 parser.add_argument('text', type=str)
 parser.add_argument('-f','--font', help="lets you pick font", action="store_true")
-parser.add_argument('font', help="write name of font", type=str)
+parser.add_argument('fontyes', help="write name of font", type=str)
 
 args = parser.parse_args()
 if args.font == 1:
-    figlet.setFont(font=args.font)
+    figlet.setFont(font=args.fontyes)
     print(
         figlet.renderText(args.text))
 else:
