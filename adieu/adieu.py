342b948abd87b7779f641aa2a24c99d3cf3f897e
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-18 11:50:07 +0000

    Sat, Apr 18, 2026, 1:50 PM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -3,6 +3,8 @@ list_of_names=[]
 
 while(True):
     try:
-
+        inputted_name = input("Name: ")
+        list_of_names.append(inputted_name)
     except EOFError:
-        continue
+        break
+
