0bf6ff4417303931b0901a86d7ae1efb4c10a7ff
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-22 15:23:05 +0000

    Wed, Apr 22, 2026, 5:23 PM +02:00

--- game/game.py
+++ game/game.py
@@ -1,16 +1,20 @@
 import random
 def main():
     random.seed()
-    userInputLimit = input("Level: ",int)
-    print(userInputLimit)
-    #randomNumber = random.randint(1,userInputLimit)
-    # while(True):
-    #     userInputGuess = ("Guess: ")
-    #     if userInputGuess == randomNumber:
-    #         print("Just right!")
-    #     elif userInputGuess > randomNumber:
-    #         print("Too large!")
-    #     elif userInputGuess < randomNumber:
-    #         print("Too large!")
+    while(True):
+        try:
+            userInputLimit = int(input("Level: "))
+        except ValueError:
+            continue
+        break
+    randomNumber = random.randint(1,userInputLimit)
+    while(True):
+        userInputGuess = input("Guess: ")
+        if userInputGuess == randomNumber:
+            print("Just right!")
+        elif userInputGuess > randomNumber:
+            print("Too large!")
+        elif userInputGuess < randomNumber:
+            print("Too large!")
 
 main()
