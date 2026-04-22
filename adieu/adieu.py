3a28ce84e281477c23416d670df1cdf6f5436647
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-04-22 13:22:23 +0000

    Wed, Apr 22, 2026, 3:22 PM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -2,8 +2,7 @@ import sys
 list_of_names=[]
 adieu_string = "Adieu, adieu, to "
 in_list_name_counter = 0
-arr = ['a','b']
-print(' '.join(str(j) for j in arr))
+
 while(True):
     try:
         inputted_name = input("Name: ")
@@ -17,9 +16,5 @@ if not list_of_names:
 
 
 
-# for i in range(len(list_of_names)-1):
-#     if i == 0:
-#         adieu_string += list_of_names[i]
-#     else:
-#         ", ".join()
-# print(adieu_string)
+
+print(f"{list_of_names[1]}{", ".join(list_of_names)}")
