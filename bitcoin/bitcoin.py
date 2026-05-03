8f3567df010f1e4644b286cf94fa812aedc3ed91
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-03 14:55:45 +0000

    Sun, May 3, 2026, 4:55 PM +02:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -8,6 +8,11 @@ parser.add_argument('amount',help="lets you pick font", type=float)  #add font a
 def main():
     try:
         args = parser.parse_args()
+        if args.amount is None:
+            raise ValueError
     except SystemExit as e:
+        print("Missing command-line argument")
+    except ValueError as e:
         
+
 main()
