aaf890a3b30caefa5fe22982b18d9294424c2630
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-03 14:46:26 +0000

    Sun, May 3, 2026, 4:46 PM +02:00

---
+++ bitcoin/bitcoin.py
@@ -0,0 +1,7 @@
+import argparse
+import sys
+import requests
+
+parser = argparse.ArgumentParser() #create praser from argprase library
+parser.add_argument('amount',help="lets you pick font", type=float)  #add font argument as a
+
