257f43c72c986e6f904c0841e011858f1ec9b6e6
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-03 15:43:13 +0000

    Sun, May 3, 2026, 5:43 PM +02:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -21,7 +21,7 @@ def main():
     try:
         r = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=25406b28524673f63ad7bc9c901b056265b01de076fd9ef72dc6eebb30f5b905")
         response = r.json()
-        print(response["priceUsd"])
+        print(response["data.priceUsd"])
     except requests.RequestException:
         print("Api error")
         sys.exit(3)
