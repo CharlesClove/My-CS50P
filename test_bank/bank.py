322d59c8c9fcfad63160b54b92f30ed2b37a20c9
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-06 11:49:57 +0000

    Wed, May 6, 2026, 1:49 PM +02:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -7,8 +7,7 @@ def value(greeting):
         return '$0'
     elif greeting[0] == 'h' or greeting[0] == 'H':
         return '$20'
-    else:
-        return '$100'
+    
 
 if __name__ == "__main__":
     main()
