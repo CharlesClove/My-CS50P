ed95ce176fe4047aae35342b46ffd55acd78fa8f
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-06 12:06:51 +0000

    Wed, May 6, 2026, 2:06 PM +02:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -4,8 +4,10 @@ import pytest
 def test_hello():
     assert value("hello") == '$0'
     assert value("Hello") == '$0'
-#def test_h():
+def test_h():
     assert value("hey") == '$20'
-    assert value("Hola") == '$20'
-#def test_else():
+    assert value("Hey") == '$20'
+def test_else():
     assert value("What's up") == '$100'
+    assert value("what's up") == '$100'
+
