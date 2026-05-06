714436f7167514f5591939267dde795abfba29d9
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-06 12:16:25 +0000

    Wed, May 6, 2026, 2:16 PM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,15 +1,15 @@
 def main():
     greeting = input("Greeting: ")
-    print(value(greeting))
+    print(f"${value(greeting)}")
 
 def value(greeting):
     greeting = greeting.lower().strip()
     if greeting.startswith('hello'):
-        return '$0'
+        return 0
     elif greeting[0] == "h":
-        return '$20'
+        return 20
     else:
-        return '$100'
+        return 100
 
 if __name__ == "__main__":
     main()
--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -2,12 +2,12 @@ from bank import value
 import pytest
 
 def test_hello():
-    assert value("hello") == '$0'
-    assert value("Hello") == '$0'
+    assert value("hello") == 0
+    assert value("Hello") == 0
 def test_h():
-    assert value("hey") == '$20'
-    assert value("Hey") == '$20'
+    assert value("hey") == 20
+    assert value("Hey") == 20
 def test_else():
-    assert value("What's up") == '$100'
-    assert value("what's up") == '$100'
+    assert value("What's up") == 100
+    assert value("what's up") == 100
 
