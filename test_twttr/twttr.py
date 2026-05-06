0e6b2afddc40149251663de64e50e2d9c7f35b17
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-06 10:42:59 +0000

    Wed, May 6, 2026, 12:42 PM +02:00

---
+++ test_twttr/twttr.py
@@ -0,0 +1,23 @@
+
+def shorten(var_string):
+
+    vovels = "aeiouAEIOU"
+    var_cut_string = ""
+
+    for iteration in range(len(var_string)):
+        print(f"{vovels.find(var_string[iteration])} To jest wynik dla vovels.find ")
+        if vovels.find(var_string[iteration]) != -1: #vovels.find() != -1 returns true if character is in string vovels
+            var_cut_string += var_string[iteration].replace(var_string[iteration], "")
+        else:
+            var_cut_string += var_string[iteration]
+
+    return var_cut_string
+
+
+def main():
+    to_cut = input("Input: ")
+    cut_string = shorten(to_cut)
+    print(f"Output: {cut_string}")
+
+if __name__ == "__main__":
+    main()
