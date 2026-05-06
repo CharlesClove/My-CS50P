8e3f72f164c174ad4e15dadbc90e852aad841e0b
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-06 10:53:47 +0000

    Wed, May 6, 2026, 12:53 PM +02:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -5,7 +5,7 @@ def test_shorten():
     assert shorten("twitter") == "twttr"
     assert shorten("123") == "123"
     assert shorten("a,b,c") == ",b,c"
-def test_shorten_fail():
-    assert shorten("twitter") == "twitter"
-    assert shorten("123") == "123"
-    assert shorten("a,b,c") == "a,b,c"
+# def test_shorten_fail():
+#     assert shorten("twitter") == "twitter"
+#     assert shorten("123") == "123"
+#     assert shorten("a,b,c") == "a,b,c"
