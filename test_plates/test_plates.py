695979d0819e47cb0815cd0cf97506de492228e2
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-07 06:46:03 +0000

    Thu, May 7, 2026, 8:46 AM +02:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -8,5 +8,4 @@ def test_plates():
     assert is_valid("GOODBYE") == False
     assert is_valid("goodbye") == False
     assert is_valid("222222") == False
-    assert is_valid("2AAAAA") == False
     assert is_valid(" AAAAA") == False
