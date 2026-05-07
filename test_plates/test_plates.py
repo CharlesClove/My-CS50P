23762f5dab75f68988bf93f45a98ab10469136f0
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-07 06:34:30 +0000

    Thu, May 7, 2026, 8:34 AM +02:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -2,6 +2,7 @@ import pytest
 from plates import *
 
 def test_plates():
-    assert is_valid() == True
-    assert is_valid() == False
-    assert is_valid() == False
+    assert is_valid("HELLO") == True
+    assert is_valid("AAA222") == True
+    assert is_valid("AAA22A") == False
+    assert is_valid("GOODBYE") == False
