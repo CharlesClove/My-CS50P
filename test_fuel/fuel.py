b2e05ad80a802335baa2ec1c2ce49c78bc23a83e
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-07 07:16:34 +0000

    Thu, May 7, 2026, 9:16 AM +02:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,5 +1,10 @@
 def convert(fraction):
-    ...
+    x, y = fraction.split('/')
+    x = float(x)
+    y = float(y)
+    if x < 0 or x > y or x.is_integer() != True or y.is_integer() != True:
+        raise ValueError
+    return (x/y)*100
 
 
 def gauge(percentage):
@@ -14,17 +19,10 @@ def main():
     while(True):
         try:
             fraction = input("Fraction: ")
-            x, y = fraction.split('/')
-            x = float(x)
-            y = float(y)
-            if x < 0 or x > y or x.is_integer() != True or y.is_integer() != True:
-                raise ValueError
-            fraction = (x/y)*100
-            #print(f"{amount}  {type(amount)}")
+            fraction = convert(fraction)
         except (ValueError,ZeroDivisionError):
             continue
         break
-    #print(f"3{amount}  {type(amount)}")
     print(gauge(round(fraction)))
 
 if __name__ == "__main__":
