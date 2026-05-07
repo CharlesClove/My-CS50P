8c8df313b3c0e7d1f3e57277c33f894614cf8395
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-07 07:42:35 +0000

    Thu, May 7, 2026, 9:42 AM +02:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -6,11 +6,19 @@ def test_gague():
     assert gauge(1) == "E"
     assert gauge(25) == "25%"
     assert convert("25/100") == 25
+
+def test_valueErrors():
     with pytest.raises(ValueError):
         convert("cat/dog")
+    with pytest.raises(ValueError):
         convert("-1/100")
+    with pytest.raises(ValueError):
         convert("10/-100")
+    with pytest.raises(ValueError):
         convert("-10/-100")
+    with pytest.raises(ValueError):
         convert("10/5")
+
+def test_zeroDivision():
     with pytest.raises(ZeroDivisionError):
         convert("25/0")
