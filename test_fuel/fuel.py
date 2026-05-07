6bd6fb9dc2ca8a96832b2569f309b2c391b08bf5
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-07 07:07:25 +0000

    Thu, May 7, 2026, 9:07 AM +02:00

---
+++ test_fuel/fuel.py
@@ -0,0 +1,29 @@
+def percentage(amount):
+    #print(f"3 {amount}  {type(amount)}")
+    if amount >= 99:
+        return ("F")
+    elif amount <= 1:
+        return ("E")
+    else:
+        return (f"{amount}%")
+
+
+def main():
+    while(True):
+        try:
+            amount = input("Fraction: ")
+            x, y = amount.split('/')
+            x = float(x)
+            y = float(y)
+            if x < 0 or x > y or x.is_integer() != True or y.is_integer() != True:
+                raise ValueError
+            amount = (x/y)*100
+            #print(f"{amount}  {type(amount)}")
+        except (ValueError,ZeroDivisionError):
+            continue
+        break
+    #print(f"3{amount}  {type(amount)}")
+    print(fuel(round(amount)))
+
+if __name__ == "__main__":
+    main()
