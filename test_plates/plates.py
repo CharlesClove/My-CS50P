1b45efaddb1a51c7d6f29e63ff4d53eb4daea7f7
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-07 06:29:16 +0000

    Thu, May 7, 2026, 8:29 AM +02:00

---
+++ test_plates/plates.py
@@ -0,0 +1,20 @@
+import re
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    plate_min_max = [2,6]
+    re_pattern = r'^[A-Z]{2,}([1-9][0-9]*)?$' # 'Allow' two or more letters. Numbers are optional '()?' , however first number cant be '0'
+    if len(s)>plate_min_max[1] or plate_min_max[0]>len(s): #is the length good?
+        return False
+    else:
+        return bool(re.fullmatch(re_pattern,s)) #does the s match the regex pattern? Not using findall because i want one exact match.
+
+
+if __name__ == "__main__":
+    main()
