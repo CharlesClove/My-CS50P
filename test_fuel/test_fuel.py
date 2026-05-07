37f7f3fe9f83138ca412e883b5dc40b83a2ab8ed
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-07 07:20:30 +0000

    Thu, May 7, 2026, 9:20 AM +02:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,2 +1,8 @@
 import pytest
-from re
+from fuel import *
+
+def test_gague():
+    assert gauge(99) == "F"
+    assert gauge(0) == "E"
+    assert gauge(25) == "25%"
+    assert convert("cat/dog") == 
