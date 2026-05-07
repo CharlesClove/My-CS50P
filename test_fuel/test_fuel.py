301fef8f8738e006f8bed5955fa5aa92086c6411
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-07 07:34:00 +0000

    Thu, May 7, 2026, 9:34 AM +02:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -5,6 +5,7 @@ def test_gague():
     assert gauge(99) == "F"
     assert gauge(1) == "E"
     assert gauge(25) == "25%"
+    assert convert("25/100") == 25
     with pytest.raises(ValueError):
         convert("cat/dog")
         convert("-1/100")
