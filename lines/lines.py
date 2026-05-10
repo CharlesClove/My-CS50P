b9ad4710a46f49e4338eee4aa232d00e7953df9e
Karol G <120822127+CharlesClove@users.noreply.github.com>
2026-05-10 18:48:54 +0000

    Sun, May 10, 2026, 8:48 PM +02:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,8 +1,14 @@
-import argphrase
+import argparse
+import sys
+__parse = argparse.ArgumentParser()
+__parse.add_argument('test',nargs=1 )
+
 
 def main():
-    praser
-        ...
+    print(sys.argv)
+    args = __parse.parse_args()
+    ...
+    print(args.test)
 
 if __name__ == "__main__":
-        main()
+    main()
